076227be965ca23a9183fc10e845a5d06a23287c
Your Name <gdikshan10@gmail.com>
2024-06-10 02:47:53 +0545

    got error while deploying into vercel by the footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import SessionWraper from "./components/SessionWraper";
 
 const inter = Inter({ subsets: ["latin"] });
