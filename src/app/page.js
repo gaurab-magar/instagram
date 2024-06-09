3d47c5ac4f4d365569a587aa8d6951f875d392e1
Your Name <gdikshan10@gmail.com>
2024-06-10 02:53:08 +0545

    remove unnecessaary code from the home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,6 @@ import Feed from "./components/Feed";
 export default function Home() {
   return (
     <div>
-      <hi>
-        hi my name is gaurab thapa magar
-      </hi>
       <Feed />
     </div>
   );
